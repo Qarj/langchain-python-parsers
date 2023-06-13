5e8138612d85f527d1ee29305e027905af9314d5
Tim Buckland <tim.buckland@gmail.com>
2023-06-13 22:57:11 +0100

    refactor: separate custom prompt template

---
+++ refactor_custom_agent_chat/custom_agent_chat.py
@@ -0,0 +1,69 @@
+from dotenv import load_dotenv
+load_dotenv()
+
+from langchain.agents import Tool, AgentExecutor, LLMSingleActionAgent, AgentOutputParser
+from langchain.prompts import BaseChatPromptTemplate
+from langchain import SerpAPIWrapper, LLMChain
+from langchain.chat_models import ChatOpenAI
+from typing import List, Union
+from langchain.schema import AgentAction, AgentFinish, HumanMessage
+import re
+from getpass import getpass
+import CustomSearchTool
+from CustomPromptTemplate import CustomPromptTemplate, template
+
+search = CustomSearchTool.CustomSearchTool()
+tools = [
+    Tool(
+        name = "CustomSearchTool",
+        func=search.run,
+        description="Useful for doing some kind of search"
+    )
+]
+
+prompt = CustomPromptTemplate(
+    template=template,
+    tools=tools,
+    # This omits the `agent_scratchpad`, `tools`, and `tool_names` variables because those are generated dynamically
+    # This includes the `intermediate_steps` variable because that is needed
+    input_variables=["input", "intermediate_steps"]
+)
+
+class CustomOutputParser(AgentOutputParser):
+    
+    def parse(self, llm_output: str) -> Union[AgentAction, AgentFinish]:
+        # Check if agent should finish
+        if "Final Answer:" in llm_output:
+            return AgentFinish(
+                # Return values is generally always a dictionary with a single `output` key
+                # It is not recommended to try anything else at the moment :)
+                return_values={"output": llm_output.split("Final Answer:")[-1].strip()},
+                log=llm_output,
+            )
+        # Parse out the action and action input
+        regex = r"Action\s*\d*\s*:(.*?)\nAction\s*\d*\s*Input\s*\d*\s*:[\s]*(.*)"
+        match = re.search(regex, llm_output, re.DOTALL)
+        if not match:
+            raise ValueError(f"Could not parse LLM output: `{llm_output}`")
+        action = match.group(1).strip()
+        action_input = match.group(2)
+        # Return the action and action input
+        return AgentAction(tool=action, tool_input=action_input.strip(" ").strip('"'), log=llm_output)
+
+output_parser = CustomOutputParser()
+llm = ChatOpenAI(temperature=0, model="gpt-4")
+# LLM chain consisting of the LLM and a prompt
+llm_chain = LLMChain(llm=llm, prompt=prompt)
+
+tool_names = [tool.name for tool in tools]
+agent = LLMSingleActionAgent(
+    llm_chain=llm_chain, 
+    output_parser=output_parser,
+    stop=["\nObservation:"], 
+    allowed_tools=tool_names
+)
+
+agent_executor = AgentExecutor.from_agent_and_tools(agent=agent, tools=tools, verbose=True)
+
+agent_executor.run("Search me test engineer jobs in London.")
+
