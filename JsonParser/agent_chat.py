8cd96b6dbfab907ee71ee4dcab6e284e4f60fafe
Tim Buckland <tim.buckland@stepstone.com>
2023-06-19 11:10:00 +0100

    wip: trying to figure out azure

--- JsonParser/agent_chat.py
+++ JsonParser/agent_chat.py
@@ -1,4 +1,6 @@
+import os
 from dotenv import load_dotenv
+
 load_dotenv()
 
 from langchain.agents import Tool, AgentExecutor, LLMSingleActionAgent
@@ -8,35 +10,35 @@ from JobSearchTool import JobSearchTool
 from JsonPromptTemplate import JsonPromptTemplate
 from JsonOutputParser import JsonOutputParser
 
+# https://python.langchain.com/docs/modules/model_io/models/llms/integrations/azure_openai_example
+
 search = JobSearchTool()
 tools = [
     Tool(
-        name = "JobSearchTool",
+        name="JobSearchTool",
         func=search.run,
-        description="Useful for searching for jobs, input is jobTitle and jobLocation as a JSON object"
+        description="Useful for searching for jobs, input is jobTitle and jobLocation as a JSON object",
     )
 ]
 
 prompt = JsonPromptTemplate(
-
     tools=tools,
     # This omits the `agent_scratchpad`, `tools`, and `tool_names` variables because those are generated dynamically
     # This includes the `intermediate_steps` variable because that is needed
-    input_variables=["input", "intermediate_steps"]
+    input_variables=["input", "intermediate_steps"],
 )
 
+# deployment_name = os.getenv("OPENAI_API_DEPLOYMENT_NAME")
 
 output_parser = JsonOutputParser()
 llm = ChatOpenAI(temperature=0, model="gpt-4")
+# llm = AzureChatOpenAI(temperature=0, deployment_name=deployment_name)
 # LLM chain consisting of the LLM and a prompt
 llm_chain = LLMChain(llm=llm, prompt=prompt)
 
 tool_names = [tool.name for tool in tools]
 agent = LLMSingleActionAgent(
-    llm_chain=llm_chain, 
-    output_parser=output_parser,
-    stop=["\nObservation:"], 
-    allowed_tools=tool_names
+    llm_chain=llm_chain, output_parser=output_parser, stop=["\nObservation:"], allowed_tools=tool_names
 )
 
 agent_executor = AgentExecutor.from_agent_and_tools(agent=agent, tools=tools, verbose=True)
