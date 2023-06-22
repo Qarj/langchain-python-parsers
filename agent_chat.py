0ca419deebc4f26bd9054542b01a0a56f94e7e51
Tim Buckland <tim.buckland@gmail.com>
2023-06-22 19:13:07 +0100

    fix: journey prompt

--- agent_chat.py
+++ agent_chat.py
@@ -25,7 +25,6 @@ tools = [
 prompt = JsonPromptTemplate(tools=tools, input_variables=["input", "intermediate_steps"])
 output_parser = JsonOutputParser()
 
-
 llm = ChatOpenAI(temperature=0, model="gpt-4")
 llm_chain = LLMChain(llm=llm, prompt=prompt)
 
@@ -39,6 +38,65 @@ agent = LLMSingleActionAgent(
 
 agent_executor = AgentExecutor.from_agent_and_tools(agent=agent, tools=tools, verbose=True)
 
-result = agent_executor.run("Search me test engineer jobs in London.")
+journey_prompt = """Your name is JobBot. You are an advanced AI that helps people find jobs. You
+are currently helping a person find a job. You are currently in the middle of a conversation with a person.
+
+Latest message from person: {utterance}
+
+Guidelines:
+- Answer the person's questions on job related topics
+- Offer to do a job search for the person
+
+__START_EXAMPLES__
+{examples}
+__END_EXAMPLES__
+
+Conversation so far:
+{chat_history}
+
+Jobseeker: {utterance}
+
+JobBot:
+"""
+
+utterance = "Search me test engineer jobs in London."
+chat_history = """
+JobBot: Hey, I'm JobBot, I can help you find a job. What kind of job are you looking for?
+
+Jobseeker: Hey there JobBot. Nice to meet you.
+
+JobBot: Nice to meet you too. What kind of job are you looking for?
+"""
+examples = """
+__Example 1__
+Jobseeker: I'm bored of my Chef job. Can you suggest some types of jobs I could do?
+JobBot: Sure, here are 10 job titles that someone with a Chef's experience could do:
+1. <job title>
+2. <job title>
+...
+10. <job title>
+
+__Example 2__
+Jobseeker: Can you please search me tester jobs in Leeds?
+JobBot: Sure, here are 5 tester jobs in Leeds:
+
+1. <job title>
+2. <job title>
+...
+5. <job title>
+
+Are you interested in any of these jobs?
+""" 
+
+placeholders = []
+placeholders.append(("utterance", utterance))
+placeholders.append(("chat_history", chat_history))
+placeholders.append(("examples", examples))
+formatted_journey_prompt = journey_prompt.format(**dict(placeholders))
+
+print (formatted_journey_prompt)
+
+result = agent_executor.run(formatted_journey_prompt)
 print("\n\n")
-print(result)
+
+print("JobBot: ", result)
